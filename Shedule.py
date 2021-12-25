495fb248f99fbea55ff8e95460d6b6e503e013a5
NazarovKI <85652843+NazarovKI@users.noreply.github.com>
2021-12-25 13:41:48 +0500

    Перед тем как внести принципиальные изменения
    Собрался создать функции def для создания строк таблицы
    И использовать списки кортежей для данных.

--- Shedule.py
+++ Shedule.py
@@ -1,5 +1,11 @@
 # Получает имена святых в честь кого служба по дате
 # crummy.com/software/BeautifulSoup/bs4/doc.ru/bs4ru.html#id18
+
+
+
+
+
+
 import docx
 import bs4
 import requests
@@ -7,10 +13,11 @@ import datetime
 from docx import Document
 from bs4 import BeautifulSoup
 from requests import get
-
+from datetime import date
 document = Document()
 document.add_heading('Расписание Богослужений') # Izhitsa 36
-document.add_heading('Седмица -я по Пятидесятнице', level=2) # Izhitsa 24 bold
+sedmica = 30
+document.add_heading('Седмица'+str( sedmica)+ '-я по Пятидесятнице', level=2) # Izhitsa 24 bold
 table = document.add_table(rows=10, cols=4)
 cell = table.cell(0, 0)
 cell.text = 'Дата'
@@ -20,18 +27,29 @@ cell = table.cell(0, 2)
 cell.text = 'Время'
 cell = table.cell(0, 3)
 cell.text = 'Богослужение'
-c = 1
-a = [5,6,7,8]
+
+def next_weekday(d, weekday): 
+    days_ahead = weekday - d.weekday() 
+    if days_ahead <= 0: # Target day already happened this week
+        days_ahead += 7 
+    return d + datetime.timedelta(days_ahead) 
+d = date.today()
+next_monday = next_weekday(d, 0) # 0 = Monday, 1=Tuesday, 2=Wednesday... 
+print(next_monday)
+
+c = 0
+a = [4,5,6,7,8]
 for day in a:
-    today = str('2022-01-')+str(day)
-    href = get(r'http://www.patriarchia.ru/bu/'+today)
+#    today = str('2021-12-')+str(day)
+    href = get(r'http://www.patriarchia.ru/bu/'+str(next_monday))
     soup = BeautifulSoup(href.text, 'html.parser')
     p = soup.p.string
     c = c + 1
     r = 1
     cell =  table.cell(c, r)
     cell.text = str(p)
-
+    
+    print (p)
 #    paragraph = document.add_paragraph(p)
 
 # Программа должна исходя из текущей даты - метод
@@ -40,7 +58,7 @@ for day in a:
 # делать запросы на святых по предстоящим датам (шт.)
 # затем сохранять в таблицу
 
-document.save('new-file-name.docx')
+document.save('Седмица'+str(sedmica)+'по Пятидесятнице.docx')
 #Замена 'Свт. ' 'Мч. ' 'Прп. ' 'Вмч. ' 'Сщмч. ' 'митр. ' пробела на ;&nbsp
 # if len(p) < 56 p = p+b
 #document.save('test.docx')
