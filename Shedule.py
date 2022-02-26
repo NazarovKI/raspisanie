b2be18e85a56a6f6f9c3a04bc13c4e9fd19cf054
“NazarovKI” <kirilnazarov@gmail.com>
2022-02-26 05:37:14 +0500

    Упростил работу программы отрезал формирование документа

--- Shedule.py
+++ Shedule.py
@@ -23,62 +23,26 @@ def saint(date):
     return soup.p.string
 
 
-# Функция определяет дату следующего дня недели на выбор относительно.
-# Заданной даты. Взято https://stackoverflow.com/users/35070/phihag
-# Взято без изменений и встроено.
-def next_weekday(d, weekday):
-    days_ahead = weekday - d.weekday()
+
+# была одна мысль -- сделать функцию определяющую след. воскресение
+def next_sunday():
+    days_ahead = 6 - date.today().weekday()
     if days_ahead <= 0: # Target day already happened this week
         days_ahead += 7
-    return d + datetime.timedelta(days_ahead)
+    return date.today() + datetime.timedelta(days_ahead)
 
-# Определяем русские дни недели и дату следующего воскресения
+
+# была вторая мысль, которая привела к открытию:
+# str(date.today()) -- годится для постановки в функцию saint
+# str(date.today()+timedelta(5)) -- годится для постановки в цикл
+
+
+# Определяем русские дни
 rus_week = ('Пн.', 'Вт.', 'Ср.', 'Чт.', 'Пт.', 'Сб.', 'Вс.')
-next_sunday = next_weekday(date.today(), 6) # 0 = Monday, 1=Tuesday, 2=Wednesday...
+print(next_sunday().strftime("%d.%m.%y")+'\n'+rus_week[next_sunday().weekday()])
 
-
-print(next_sunday.strftime("%d.%m.%y")+'\n'+rus_week[next_sunday.weekday()])
-saint_of_sunday = saint (next_sunday)
-print (saint_of_sunday)
-
-
-# Создаём документ, заголовок, подзаголовок.
-document = Document()
-document.add_heading('Расписание Богослужений') # Izhitsa 36
-heading = saint_of_sunday.replace(' Неделя', 'Седмица').split('.',2)
-document.add_heading(heading[1], level=2) # Izhitsa 24 bold
-
-
-# Создаём таблицу и заголовок таблицы.
-table = document.add_table(rows=8, cols=4)
-heading_cells = table.rows[0].cells
-heading_cells[0].text = 'Дата'
-heading_cells[1].text = 'Поминаемые святые'
-heading_cells[2].text = 'Время'
-heading_cells[3].text = 'Богослужение'
-
-
-# Получаем данные для таблицы
-week = [1,2,3,4,5,6,7]
+week = [0,1,2,3,4,5,6,7,8]
 for day in week:
-    day_ = date(2022,1,2)+timedelta(day)
-    saint_of_day = saint (day_)
-    row = table.rows[day]
-    row.cells[0].text = str(day_.strftime("%d.%m.%y")+'\n'+rus_week[day_.weekday()])
-    row.cells[1].text = saint (day_)
-    row.cells[2].text = '9:00'
-    row.cells[3].text = 'Божественная Литургия'
+    saint_of_day = saint (next_sunday()+timedelta(day))
+    print (saint_of_day)
 
-
-# Помещаем данные в строку 1 по ячейкам
-row = table.rows[1]
-
-
-# Прочитываем последовательно таблицу
-for row in table.rows:
-    for cell in row.cells:
-        print(cell.text)
-
-# Сохраняем документ
-filename = heading[1].split(',')
-document.save(str(filename[0])+'.docx')
